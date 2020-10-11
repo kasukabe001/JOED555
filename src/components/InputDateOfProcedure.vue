b78a6fab05585530709c9b4eaa00fe080f14f67c
kfujita <kazuhiko.fujita@elife.co.jp>
2020-10-11 16:33:28 +0900

    10月8日Commit

---
+++ src/components/InputDateOfProcedure.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <div class="label"><span>手術日</span></div>
+    <Datepicker
+      v-model="DateOfProcedure"
+      wrapper-class="field"
+      placeholder="クリックでカレンダー"
+      :typeable="true"
+      format="yyyy-MM-dd"
+      :input-class="RequiredClass"
+      :language="DatepickerTranslation"
+      :disabled="disabled"
+      >
+    </Datepicker>
+  </div>
+</template>
+
+<script>
+import Datepicker from 'vuejs-datepicker'
+import { ja } from 'vuejs-datepicker/dist/locale'
+
+export default {
+  name: 'InputDateOfProcedure',
+  props: {
+    value: {},
+    required: {
+      default: false
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  components: {
+    Datepicker
+  },
+  data () {
+    return ({
+      DatepickerTranslation: ja
+    })
+  },
+  computed: {
+    DateOfProcedure: {
+      get () { return this.value },
+      set (newvalue) {
+        // Datepickerから渡されるnewvalueはISO datestrなので整形が必要
+        const dateobj = new Date(newvalue)
+        const datestr = dateobj.getFullYear() + '-' +
+          ('0' + (dateobj.getMonth() + 1)).slice(-2) + '-' +
+          ('0' + dateobj.getDate()).slice(-2)
+        this.$emit('input', datestr)
+      }
+    },
+    RequiredClass () {
+      return (this.required === true && this.value === '') ? 'vacant' : ''
+    }
+  }
+}
+</script>
